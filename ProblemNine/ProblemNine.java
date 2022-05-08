288452a2ecbaaf6500c97fd8b5e353e6677980e0
Sean <smriedl@gmail.com>
2022-05-08 18:27:32 -0400

    Refactor ProblemNine readData function, move processData into Data class.

--- ProblemNine/ProblemNine.java
+++ ProblemNine/ProblemNine.java
@@ -12,19 +12,32 @@ public class ProblemNine {
 
     dataList = readData();
     for(int i = 0; i < dataList.size(); i++) {
-      System.out.println("Read " + (i + 1) + " records");
-      ArrayList<Double> result = new ArrayList<Double>();
+      System.out.println("Reading Sample: " + (i + 1));
+      System.out.println("*" + dataList.get(i).ctrlRecord[0] + " " + dataList.get(i).ctrlRecord[1]);
       for(int j = 0; j < dataList.get(i).data.size(); j++) {
-        // Double val1 = dataList.get(i).data.get(j).val1;
-        // Double val2 = dataList.get(i).data.get(j).val2;
-        // Double val3 = dataList.get(i).data.get(j).val3;
-
-        // result.add(processData(val1, val2, val3, df)[1]);
-        System.out.println(' ');        
+        System.out.println(dataList.get(i).data.get(j).values);
       }
-      double average = result.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
-      System.out.println("The resulting form average is " + df.format(average));
-      System.out.println(' ');
+
+      if(!dataList.get(i).verifyNumberOfElements() || !dataList.get(i).verifyNumberOfRecords()) {
+        System.out.println("Sample rejected due to discrepancies in data counts.");
+      } else {
+        ArrayList<Double> runningResults = new ArrayList<Double>();
+        for(int j = 0; j < dataList.get(i).data.size(); j++) {
+          System.out.println();
+          System.out.println(dataList.get(i).data.get(j).values);
+
+          dataList.get(i).data.get(j).processData();
+          dataList.get(i).data.get(j).printResults(df);
+
+          if(!dataList.get(i).data.get(j).results[1].isNaN()) {
+            runningResults.add(dataList.get(i).data.get(j).results[1]);
+          }
+        }
+        double average = runningResults.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+        System.out.println();
+        System.out.println("The resulting average of the form value is " + df.format(average));
+      }
+      System.out.println();
     }
   }
 
@@ -35,7 +48,7 @@ public class ProblemNine {
     Scanner scanner = new Scanner(file);
     scanner.useDelimiter(" ");
     int count = 0;
-    int numberOfSamples = Integer.parseInt(scanner.nextLine());
+    int numberOfSamples = Integer.parseInt(scanner.nextLine().split(" ")[0]);
     String currentLine = null;
 
     first:
@@ -50,87 +63,39 @@ public class ProblemNine {
         ArrayList<Data> data = new ArrayList<Data>();
         int numberOfRecords = Integer.parseInt("0" + currentLine.charAt(1));
         int numberOfElements = Integer.parseInt("0" + currentLine.charAt(3));
-        // @todo: Revise section to read lines until * is the first character
-        //        then break out of the if statement. Lines with * should be processed
-        //        as Data object and added to ArrayList data. Before breaking out of 
-        //        if statement create DataGroup object and add it to ArrayList dataList
-        for(int i = 0; i < numberOfRecords; i++) {
-          String[] line = scanner.nextLine().split(" ");
-          if(count == numberOfSamples) {
-            currentLine = null;
-            continue first;
-          }
-          if(line[0].charAt(0) == '*') {
-            System.out.println("Data set " + count + " was rejected.");
-            currentLine = String.join(" ", line);
+        String[] line = scanner.nextLine().split(" ");
 
-            continue first;
-          } else if(line.length != numberOfElements) {
-            System.out.println("Data set " + count + " was rejected.");
-            currentLine = null;
-            continue first;
-          } else if(line[0].charAt(0) == 'a' || line[0].charAt(0) == 'b') {
-            System.out.println("Data set " + count + " was rejected.");
-            continue first;
-          }
-          
-          Double val1 = Double.parseDouble(line[0]);
-          Double val2 = Double.parseDouble(line[1]);
-          Double val3 = null;
+        while(line[0].charAt(0) != '*') {
+          Object[] lineData = new Object[line.length];
 
-          if(line.length == 3) {
-            switch(line[2]) {
-              case "a": val3 = 3.21;
-                        break;
-              case "b": val3 = 4.1;
-                        break;
-              case "c": val3 = 6.8;
-                        break;
-              default: val3 = 3.21;
-                        break;
-            }
+          for(int i = 0; i < line.length; i++) {
+            Object val = line[i];
+            lineData[i] = val;
           }
-          
-          Data currData = new ProblemNine().new Data(val1, val2, val3);
+          Data currData = new ProblemNine().new Data(lineData);
           data.add(currData);
+
+          if(scanner.hasNextLine()) {
+            line = scanner.nextLine().split(" ");
+          } else {
+            break;
+          }
         }
+        currentLine = String.join(" ", line);
         int[] currControlRecord = new int[]{numberOfRecords, numberOfElements};
         DataGroup currDataGroup = new ProblemNine().new DataGroup(currControlRecord, data);
         dataList.add(currDataGroup);
+
+        if(count == numberOfSamples) {
+          currentLine = null;
+          continue first;
+        }
       }
-      currentLine = null;
     }
-    System.out.println(' ');
     scanner.close();
     return dataList;
   }
 
-  public static Double[] processData(Double val1, Double val2, Double val3, DecimalFormat df) {
-    Double avg;
-    Double form;
-
-    // Constants
-    Double exponent = 1.5;
-    Double psi = 6.84845;
-    Double zeta = 4.26;
-    if(val3 == null) {
-      avg = (val1 + val2) / 2;
-      form = (3.14 * 3.14 * val1) + (psi * (2.44 + Math.pow(val2, exponent)) * 3.0);
-      
-      System.out.println("AVG = " + df.format(avg));
-      System.out.println("FORM = " + df.format(form));
-    } else {
-      avg = (val1 + val2 + val3) / 3;
-      form = 3.14 * 3.14 * val1 * 3.14 + psi * (2.44 + Math.pow(val2, exponent)) * 3 + (Math.pow(zeta, val3)) / val2 * Math.log(val3);
-      
-      System.out.println("AVG 33 = " + df.format(avg));
-      System.out.println("FORM 33 = " + df.format(form));
-    }
-    Double[] results = new Double[] {avg, form};
-    return results;
-  }
-
-  // move verification to DataGroup
   public class DataGroup {
     public int[] ctrlRecord;
     public ArrayList<Data> data;
@@ -142,7 +107,7 @@ public class ProblemNine {
 
     public Boolean verifyNumberOfElements() {
       for(int i = 0; i < this.data.size(); i++) {
-        if(this.data.get(i).size() != this.ctrlRecord[1]) {
+        if(this.data.get(i).values.size() != this.ctrlRecord[1]) {
           return false;
         }
       }
@@ -158,33 +123,60 @@ public class ProblemNine {
 
   public class Data {
     public ArrayList<Double> values;
+    public Double[] results = new Double[2];
 
-    public Data(Object... args) {
+    // Constants
+    private Double exponent = 1.5;
+    private Double psi = 6.84845;
+    private Double zeta = 4.26;
+
+    public Data(Object[] args) {
       this.values = new ArrayList<Double>();
-      for(Object arg : args) {
-        Double value;
-        if(arg.getClass().getName() == "String") {
-          switch(arg.toString()) {
-            case "a": value = 3.21;
-                      break;
-            case "b": value = 4.1;
-                      break;
-            case "c": value = 6.8;
-                      break;
-            default:  value = 3.21;
-                      break;
+      for(int i = 0; i < args.length; i++) {
+        Double value = null;
+        try {
+          value = Double.parseDouble(args[i].toString());
+        } catch (NumberFormatException e) {
+          if(args[i].getClass().getName().equals("java.lang.String")) {
+            switch(args[i].toString()) {
+              case "a": value = 3.21;
+                        break;
+              case "b": value = 4.1;
+                        break;
+              case "c": value = 6.8;
+                        break;
+              default:  value = 3.21;
+                        break;
+            }
           }
-          this.values.add(value);
-          continue;
         }
-        System.out.println(this.values);
-        value = Double.parseDouble(arg.toString());
         this.values.add(value);
       }
     }
 
-    public int size() {
-      return this.values.size();
+    public void processData() {
+      Double avg;
+      Double form;
+      if(this.values.size() == 2) {
+        avg = (this.values.get(0) + this.values.get(1)) / 2;
+        form = (3.14 * 3.14 * this.values.get(0)) + (psi * (2.44 + Math.pow(this.values.get(1), exponent)) * 3.0);
+
+      } else {
+        avg = (this.values.get(0) + this.values.get(1) + this.values.get(2)) / 3;
+        form = 3.14 * 3.14 * this.values.get(0) * 3.14 + psi * (2.44 + Math.pow(this.values.get(1), exponent)) * 3 + (Math.pow(zeta, this.values.get(2))) / this.values.get(1) * Math.log(this.values.get(2));
+      }
+      this.results[0] = avg;
+      this.results[1] = form;
+    }
+
+    public void printResults(DecimalFormat df) {
+      if(this.values.size() == 2) {
+        System.out.println("AVG = " + df.format(this.results[0]));
+        System.out.println("FORM = " + df.format(this.results[1]));
+      } else {
+        System.out.println("AVG 33 = " + df.format(this.results[0]));
+        System.out.println("FORM 33 = " + df.format(this.results[1]));
+      }
     }
   }
 }
--- ProblemNineTest/ProblemNineTest.java
+++ ProblemNineTest/ProblemNineTest.java
@@ -1,7 +1,6 @@
 package ProblemNineTest;
 import java.util.ArrayList;
 import ProblemNine.ProblemNine;
-import java.text.DecimalFormat;
 
 public class ProblemNineTest {
   static int totalTests = 0;
@@ -13,12 +12,14 @@ public class ProblemNineTest {
   public static void dataGroupReturnsFalseWhenIncorrectNumberOfElements() {
     totalTests += 1;
 
-    ProblemNine.Data testData1 = problemNine.new Data(1d, 2d, 3d);
-    ProblemNine.Data testData2 = problemNine.new Data(1d, 2d, 3d);
+    Object[] testArray = {1d, 2d, 3d};
+    Object[] testArray2 = {6d, 4d, 9d};
+    ProblemNine.Data testData1 = problemNine.new Data(testArray);
+    ProblemNine.Data testData2 = problemNine.new Data(testArray2);
     ArrayList<ProblemNine.Data> testList = new ArrayList<ProblemNine.Data>();
     testList.add(testData1);
     testList.add(testData2);
-    ProblemNine.DataGroup testDataGroup = problemNine.new DataGroup(new int[] {1,2}, testList);
+    ProblemNine.DataGroup testDataGroup = problemNine.new DataGroup(new int[] { 1, 2 }, testList);
 
     if(testDataGroup.verifyNumberOfElements()) {
       System.out.println("Verify Elements test failed");
@@ -29,16 +30,19 @@ public class ProblemNineTest {
   }
 
   // Test
-  public static void dataGroupReturnsTrueWhenCorrectNumberOfElements() {
+  public static void dataGroupReturnsTrueWithCorrectNumberOfElements() {
     totalTests += 1;
 
-    ProblemNine.Data testData1 = problemNine.new Data(1d, 2d, 3d);
-    ProblemNine.Data testData2 = problemNine.new Data(1d, 2d, 3d);
+    Object[] testArray = {1d, 2d, 3d};
+    Object[] testArray2 = {6d, 4d, 9d};
+
+    ProblemNine.Data testData1 = problemNine.new Data(testArray);
+    ProblemNine.Data testData2 = problemNine.new Data(testArray2);
     ArrayList<ProblemNine.Data> testList = new ArrayList<ProblemNine.Data>();
     testList.add(testData1);
     testList.add(testData2);
 
-    ProblemNine.DataGroup testDataGroup = problemNine.new DataGroup(new int[] { 1 , 3 }, testList);
+    ProblemNine.DataGroup testDataGroup = problemNine.new DataGroup(new int[] { 1, 3 }, testList);
 
     if(testDataGroup.verifyNumberOfElements()) {
       passedTests += 1;
@@ -49,61 +53,106 @@ public class ProblemNineTest {
   }
 
     // Test
-    public static void dataGroupReturnsFalseWhenIncorrectNumberOfRecords() {
+    public static void dataGroupReturnsFalseWithIncorrectNumberOfRecords() {
       totalTests += 1;
 
-      ProblemNine.Data testData1 = problemNine.new Data(1d, 2d, 3d);
-      ProblemNine.Data testData2 = problemNine.new Data(1d, 2d, 3d);
+      Object[] testArray = {1d, 2d, 3d};
+      ProblemNine.Data testData1 = problemNine.new Data(testArray);
+      ProblemNine.Data testData2 = problemNine.new Data(testArray);
       ArrayList<ProblemNine.Data> testList = new ArrayList<ProblemNine.Data>();
       testList.add(testData1);
       testList.add(testData2);
       ProblemNine.DataGroup testDataGroup = problemNine.new DataGroup(new int[] {1,2}, testList);
   
-      if(testDataGroup.verifyNumberOfElements()) {
-        System.out.println("Verify Elements test failed");
+      if(testDataGroup.verifyNumberOfRecords()) {
+        System.out.println("Verify Records test failed");
         failedTests += 1;
       } else {
         passedTests += 1;
       }
     }
 
-    // Test
-    public static void processDataReturnsCorrectValueWithTwoArguments() {
-      totalTests +=1;
-      DecimalFormat df = new DecimalFormat("0.000");
+        // Test
+        public static void dataGroupReturnsTrueWithCorrectNumberOfRecords() {
+          totalTests += 1;
+    
+          Object[] testArray = {1d, 2d, 3d};
+          ProblemNine.Data testData1 = problemNine.new Data(testArray);
+          ProblemNine.Data testData2 = problemNine.new Data(testArray);
+          ArrayList<ProblemNine.Data> testList = new ArrayList<ProblemNine.Data>();
+          testList.add(testData1);
+          testList.add(testData2);
+          ProblemNine.DataGroup testDataGroup = problemNine.new DataGroup(new int[] {2,2}, testList);
+      
+          if(testDataGroup.verifyNumberOfRecords()) {
+            passedTests += 1;
+          } else {
+            System.out.println("Verify Records test failed");
+            failedTests += 1;
+          }
+        }
 
-      if((ProblemNine.processData(5d, 3d, null, df)[0].equals(4.000) && ProblemNine.processData(5d, 3d, null, df)[1].equals(206.18542417785568)) &&
-      (ProblemNine.processData(8d, 13d, null, df)[0].equals(10.500) && ProblemNine.processData(8d, 13d, null, df)[1].equals(1092.0125216656029415040309046791244005610829888248685805920)) && 
-      (ProblemNine.processData(72d, 54d, null, df)[0].equals(63.000) && ProblemNine.processData(72d, 54d, null, df)[1].equals(8912.7729560762396386472063990099070850082678096169418549575))) {
+    // Test
+    public static void processDataReturnsCorrectValuesWithTwoArguments() {
+      totalTests +=1;
+
+      Object[] testArray = {5d, 3d};
+      Object[] testArray2 = {8d, 13d};
+      Object[] testArray3 = {3d, 11d};
+  
+      ProblemNine.Data testData1 = problemNine.new Data(testArray);
+      ProblemNine.Data testData2 = problemNine.new Data(testArray2);
+      ProblemNine.Data testData3 = problemNine.new Data(testArray3);
+
+      testData1.processData();
+      testData2.processData();
+      testData3.processData();
+
+      if((testData1.results[0].equals(4.000) && testData1.results[1].equals(206.18542417785568)) &&
+      (testData2.results[0].equals(10.500) && testData2.results[1].equals(1092.0125216656029415040309046791244005610829888248685805920)) && 
+      (testData3.results[0].equals(7.0) && testData3.results[1].equals(829.26284250181145719014831631492801927783549515136648517862))) {
         passedTests += 1;
       } else {
         failedTests +=1;
-        System.out.println("Process Data test failed");
+        System.out.println("Two Value Process Data test failed");
       }
     };
 
-      // Test
-      public static void processDataReturnsCorrectValueWithThreeArguments() {
-        totalTests +=1;
-        DecimalFormat df = new DecimalFormat("0.000");
 
-        if((ProblemNine.processData(5d, 3d, 3.21, df)[0].equals(3.736666666666667) && ProblemNine.processData(5d, 3d, 3.21, df)[1].equals(352.42895943699545040893995231727278386626170745554408681829)) &&
-        (ProblemNine.processData(12d, 7d, 4.1, df)[0].equals(7.7) && ProblemNine.processData(12d, 7d, 4.1, df)[1].equals(878.8825012545553)) && 
-        (ProblemNine.processData(22d, 50d, 6.8, df)[0].equals(26.266666666666666) && ProblemNine.processData(22d, 50d, 6.8, df)[1].equals(8725.6093525359887186102046896571241572821700459892825129853))) {
-          passedTests += 1;
-        } else {
-          failedTests +=1;
-          System.out.println("Process Data test failed");
-        }
-      };
-    
+    // Test
+    public static void processDataReturnsCorrectValuesWithThreeArguments() {
+      totalTests +=1;
+
+      Object[] testArray = {5d, 3d, 3.21};
+      Object[] testArray2 = {12d, 7d, 4.1};
+      Object[] testArray3 = {22d, 50d, 6.8};
+  
+      ProblemNine.Data testData1 = problemNine.new Data(testArray);
+      ProblemNine.Data testData2 = problemNine.new Data(testArray2);
+      ProblemNine.Data testData3 = problemNine.new Data(testArray3);
+
+      testData1.processData();
+      testData2.processData();
+      testData3.processData();
+
+      if((testData1.results[0].equals(3.736666666666667) && testData1.results[1].equals(352.42895943699545040893995231727278386626170745554408681829)) &&
+      (testData2.results[0].equals(7.7) && testData2.results[1].equals(878.8825012545553)) && 
+      (testData3.results[0].equals(26.266666666666666) && testData3.results[1].equals(8725.6093525359887186102046896571241572821700459892825129853))) {
+        passedTests += 1;
+      } else {
+        failedTests +=1;
+        System.out.println("Three Value Process Data test failed");
+      }
+    };
 
   public static void main(String[] args) {
     dataGroupReturnsFalseWhenIncorrectNumberOfElements();
-    dataGroupReturnsTrueWhenCorrectNumberOfElements();
-    dataGroupReturnsFalseWhenIncorrectNumberOfRecords();
-    processDataReturnsCorrectValueWithTwoArguments();
-    processDataReturnsCorrectValueWithThreeArguments();
+    dataGroupReturnsTrueWithCorrectNumberOfElements();
+    dataGroupReturnsFalseWithIncorrectNumberOfRecords();
+    dataGroupReturnsTrueWithCorrectNumberOfRecords();
+
+    processDataReturnsCorrectValuesWithTwoArguments();
+    processDataReturnsCorrectValuesWithThreeArguments();
 
     System.out.println("Total Test: " + totalTests);
     System.out.println("Passed Tests: " + passedTests);
